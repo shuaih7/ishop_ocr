3787c69f40f0ef3766d297663f5d9f96abd1becc
Shuaih7 <shuaih7@gmail.com>
2021-01-14 01:35:48 +0800

    feat: infer models added

--- HMI.py
+++ HMI.py
@@ -3,7 +3,7 @@
 
 '''
 Created on 01.13.2021
-Updated on 12.31.2021
+Updated on 01.14.2021
 
 Author: haoshaui@handaotech.com
 '''
@@ -20,6 +20,7 @@ import glob as gb
 abs_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(abs_path)
 
+from paddleocr import PaddleOCR
 from PyQt5.uic import loadUi
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtGui import QImage, QPixmap, QFont
@@ -42,7 +43,7 @@ class MainWindow(QMainWindow):
         with open (config_file, "r") as f: 
             self.config_matrix = json.load(f)
             
-        self.mode = self.config_matrix["mode"]
+        self.mode = self.config_matrix["Global"]["mode"]
         self.imageLabel.setConfig(self.config_matrix)
         
         # Config the devices
@@ -58,6 +59,9 @@ class MainWindow(QMainWindow):
             
         # Initialization
         self.image = None
+        self.part_list = []
+        self.scan_dict = {}
+        self.initModels()
             
     def liveStream(self):
         if self.mode.lower() == "test":
@@ -69,7 +73,71 @@ class MainWindow(QMainWindow):
         else:
             # TODO: add camera functions here ...
             QApplication.processEvents()
+            
+    def initModels(self):
+        params_doc = self.config_matrix["Model_DOC"]
+        params_doc["build_params"]["use_gpu"] = self.config_matrix["Global"]["use_gpu"]
+        params_ocr = self.config_matrix["Model_OCR"]
+        params_ocr["build_params"]["use_gpu"] = self.config_matrix["Global"]["use_gpu"]
         
+        self.model_doc = PaddleOCR(**params_doc["build_params"])
+        self.model_ocr = PaddleOCR(**params_ocr["build_params"])
+    
+    @pyqtSlot()    
+    def recParts(self):
+        if len(self.scan_dict) == 0:
+            QMessageBox.warning(self,"警告", "请先扫描交接单！", QMessageBox.Yes)
+        else:
+            self.part_list = []
+            image = self.image
+            params = self.config_matrix["Model_OCR"]["infer_params"]
+            results = self.model_doc.ocr(image, **params)
+            
+            for result in results:
+                self.part_list.append(result[1][0])
+            
+            self.updateTable()
+
+    @pyqtSlot()        
+    def recDocument(self):
+        self.scan_dict = {}
+        #image = self.image
+        image = cv2.imread(r"E:\Projects\Part_Number\dataset\20210113\test\doc.bmp", cv2.IMREAD_COLOR)
+        params = self.config_matrix["Model_DOC"]["infer_params"]
+        results = self.model_doc.ocr(image, **params)
+        
+        for result in results:
+            number = result[1][0]
+            if self.isValidNumber(number): 
+                 self.scan_dict[number] = {"position": None, "status": None}
+        
+        self.updateTable()
+
+    @pyqtSlot()
+    def createReport(self):
+        pass
+        
+    @pyqtSlot()
+    def clearAll(self):
+        self.scan_dict = {}
+        self.part_list = []
+        
+    def updateTable(self):
+        pass
+        
+    def isValidNumber(self, number):
+        if len(number)>=10 and number[4]=="M":
+            return True
+        else:
+            return False
+            
+    def formatNumber(self, number):
+        number = number.replace("O", "0")
+        number = number.replace("o", "0")
+        number = number.replace("I", "1")
+        if number[-3] == "2": number = number[:-4]+"Z"+number[-2:]
+        return number
+     
     def message(self, msg, flag="info"): 
         self.logger_flags[flag](msg)
         
@@ -84,7 +152,7 @@ class MainWindow(QMainWindow):
         if reply == QMessageBox.Yes: 
             #if self.isInferring: self.isInferring = False
             #if self.isRunning: self.isRunning = False
-            self.message("ishopOCR UI 已关闭。\n", flag="info")
+            self.message("i-shopOCR user interface has been closed。\n", flag="info")
             sys.exit()
             #ev.accept()
         else: ev.ignore()
--- pytest.py
+++ pytest.py
@@ -1,7 +1,22 @@
 import os, sys
 import numpy as np
 
-A = np.array([1,2,3,4,5])
-print(type(A))
+params = {
+    "hobby": "soccer",
+    "name": "Shuai",
+    "age": 27,
+}
 
-if isinstance(A, np.ndarray): print("Yeah!")
+
+def build_person(id, name, age, hobby):
+    print("Person's ID:   ", id)
+    print("Person's name: ", name)
+    print("Person's age:  ", age)
+    print("Person's hobby:", hobby)
+    
+    
+if __name__ == "__main__":
+    #build_person("212780558", **params)
+    number = "12345679"
+    number = number[:-4]+"Z"+number[-2:]
+    print(number)
