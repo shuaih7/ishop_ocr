951d2b60c21e5b0155cca37f9f91b427fe0545a2
Shuaih7 <shuaih7@gmail.com>
2021-01-21 14:30:33 +0800

    feat: result merge

--- utils/enhancement.py
+++ utils/enhancement.py
@@ -10,7 +10,12 @@ class SNPatch():
     '''
     Get SN patches for OCR recognition
     '''
-    offset = (233, 568)  #(r,c)
+    # offset = (500, 300)  #(r,c)
+    # width = 905
+    # height = 683
+    # row_nbr, col_nbr = 4, 5
+
+    offset = (0, 0)  #(r,c)
     width = 905
     height = 683
     row_nbr, col_nbr = 5, 5
@@ -18,17 +23,27 @@ class SNPatch():
     def __init__(self):
         self.image_patches = []
         self.image_filtered = None
+        self.roi = []  # list of r0,c0,r1,c1
+        self.set_roi()
 
     # todo, roi setting for config files
     def set_roi(self):
         ''''''
-        pass
+        for r in range(self.row_nbr):
+            for c in range(self.col_nbr):
+                r0, r1 = self.offset[0] + self.height * r, self.offset[0] + self.height * (r + 1)
+                c0, c1 = self.offset[1] + self.width * c, self.offset[1] + self.width * (c + 1)
+                self.roi.append((r0,c0,r1,c1))
+                print("r0,c0,r1,c1: ",r0, c0, r1, c1)
 
     #
     def __call__(self, image, engine=None, params={}, app=None):
         #self.image_filtered,_ = self.denoise(image,image)
         #return self.get_patches(self.image_filtered)
-        return self.rec_patches(image, engine, params, app)
+        #return self.rec_patches(image, engine, params, app)
+
+        self.image_filtered = median_filter(image,8)
+        return self.rec_patches(self.image_filtered , engine, params, app)
 
     def histeq(self,im,nbr_bins=256):
         imhist,bins = histogram(im.flatten(),nbr_bins,normed=True)
@@ -93,23 +108,25 @@ class SNPatch():
         height,width = self.height,self.width
         
         image_patches = []
-        for r in range(row_nbr):
-            for c in range(col_nbr):
-                r0, r1 = offset[0] + height * r, offset[0] + height * (r + 1)
-                c0, c1 = offset[1] + width * c, offset[1] + width * (c + 1)
-                # print(r0, r1, c0, c1)
-                img_patch= img_filtered[r0:r1, c0:c1]
-                image_patches.append(img_patch)
+        # for r in range(row_nbr):
+        #     for c in range(col_nbr):
+        #         r0, r1 = offset[0] + height * r, offset[0] + height * (r + 1)
+        #         c0, c1 = offset[1] + width * c, offset[1] + width * (c + 1)
+        #         print(r0, r1, c0, c1)
+        for roi in self.roi:
+            r0,c0,r1,c1 = roi
+            img_patch= img_filtered[r0:r1, c0:c1]
+            image_patches.append(img_patch)
         return image_patches
         
     def rec_patches(self, img_filtered, engine=None, params={}, app=None):
         if engine is None: return []
             
-        results = []
         row, col = 0, 0
         offy, offx = self.offset
+        results = []
         img_patches = self.get_patches(img_filtered)
-        
+        i = 0
         for img in img_patches:
             loc_result = engine.ocr(img, **params)
 
@@ -118,24 +135,49 @@ class SNPatch():
                 text = label[1][0]
                 confidence = label[1][1]
                 
+                #for point,roi in zip(points,self.roi):
+                r0, c0, r1, c1 = self.roi[i]
+                print("r0,c0,r1,c1:", r0, c0, r1, c1)
                 for point in points:
-                    point[0] += offx + col*self.width
-                    point[1] += offy + row*self.height
-                results.append([points, [text, confidence]])
-                
-            col += 1
-            if col == self.col_nbr:
-                col = 0
-                row += 1
+                    point[0] += c0
+                    point[1] += r0
+                    # point[0] += offx + col*self.width
+                    # point[1] += offy + row*self.height
+                results.append(self.merge_results([points, [text, confidence]]))
+            i += 1
+            # col += 1
+            # if col == self.col_nbr:
+            #     col = 0
+            #     row += 1
             if app is not None: app.processEvents()
 
         return results
-  
-    def _merge_text(self, results):
-        if len(results[0]) <= 1: 
-            return results
-        elif len(results[0]) == 2:
-            string1 = 0
+        
+    def merge_results(self, results):
+        if len(results) != 2: return results
+        
+        xs, ys = [], []
+        for pt in results[0][0] + results[1][0]:
+            xs.append(pt[0])
+            ys.append(pt[1])
+        
+        min = min(xs)
+        ymin = min(ys)
+        xmax = max(xs)
+        ymax = max(ys)
+        
+        # Clock-wise         
+        roi = [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]]
+
+        if len(results[0][1][0]) >= len(results[1][1][0]):
+            text = results[0][1][0] + results[1][1][0]
+        else:
+            text = results[1][1][0] + results[0][1][0]
+        confidence = (results[0][1][1] + results[1][1][1])/2
+        
+        mresult = [roi, [text, confidence]]
+        
+        return mresult
 
 
 if __name__ =="__main__":
